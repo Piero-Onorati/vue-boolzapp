19e88fcd7bff328f6762e0d3734653fd206dec4b
Piero <piero.onorati@virgilio.it>
2021-06-29 14:02:58 +0200

    add milestone 2 with v-for

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const app = new Vue(
 
             myMessage: 'my-message',
             userMessage : 'user-message',
+            counter :0,
             newMessage : '',
             search: '',
             contacts: [
@@ -124,42 +125,43 @@ const app = new Vue(
             ]
         },
 
+       
+
         methods: {
+
+            show : function (index){
+                this.counter = index
+            }
+                
             
-            show: function(element,index){
+            // Milestone 2 con backtick
+            // show: function(element,index){
 
-                document.getElementById('all-messages').innerHTML = '';
+            //     document.getElementById('all-messages').innerHTML = '';
 
-                console.log(element.name);
+            //     console.log(element.name);
 
-                document.getElementById('user-info').innerHTML =
-                    `<div class="circle-div">
-                        <img src="${element.avatar}" alt="">
-                    </div>
-                    <div class="name-last-access">
-                        <h3>${element.name}</h3>
-                        <span class="last-access">Ultimo accesso oggi alle</span>
-                    </div>`;
+            //     document.getElementById('user-info').innerHTML =
+            //         `<div class="circle-div">
+            //             <img src="${element.avatar}" alt="">
+            //         </div>
+            //         <div class="name-last-access">
+            //             <h3>${element.name}</h3>
+            //             <span class="last-access">Ultimo accesso oggi alle</span>
+            //         </div>`;
 
-                element.messages.forEach(element => {
-                    console.log(element.text);
-                    let messageType = (element.status == 'sent')? this.myMessage : this.userMessage;
-                    document.getElementById('all-messages').innerHTML += 
-                    `<div class="message ${messageType}">
-                    <h4>${element.text}</h4>
-                    <span class="last-message-date">${element.date}</span>
-                    </div>`                    
-                });
+            //     element.messages.forEach(element => {
+            //         console.log(element.text);
+            //         let messageType = (element.status == 'sent')? this.myMessage : this.userMessage;
+            //         document.getElementById('all-messages').innerHTML += 
+            //         `<div class="message ${messageType}">
+            //         <h4>${element.text}</h4>
+            //         <span class="last-message-date">${element.date}</span>
+            //         </div>`                    
+            //     });
                    
-            },
+            // },
 
-            send: function(){
-                this.contacts.messages.forEach(element => {
-                    console.log(element.message);
-                                      
-                });
-
-            },
         }
 
         
