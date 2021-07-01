2b6b6860bb13d55c8db437dba41ff36f6fea73c4
Piero <piero.onorati@virgilio.it>
2021-07-01 22:47:23 +0200

    add blue tick but with no synchronicity

--- js/script.js
+++ js/script.js
@@ -13,7 +13,8 @@ const app = new Vue(
             search: '',
             active: false,
             indexCheck: '',
-            onlyReceived:'last-message-date-onlyreceived::after',
+            received : 'display-inline',
+            notReceived : 'display-none',
             contacts: [
                 {
                     name: 'Michela',
@@ -23,17 +24,20 @@ const app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             text: 'Hai portato a spasso il cane?',
-                            status: 'sent'
+                            status: 'sent',
+                            check: 'received',
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             text: 'Ricordati di dargli da mangiare',
-                            status: 'sent'
+                            status: 'sent',
+                            check: 'received',
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             text: 'Tutto fatto!',
-                            status: 'received'
+                            status: 'received',
+                            check: 'na',
                         }
                     ]
                 },
@@ -46,17 +50,20 @@ const app = new Vue(
                         {
                             date: '20/03/2020 16:30:00',
                             text: 'Ciao come stai?',
-                            status: 'sent'
+                            status: 'sent',
+                            check: 'received',
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             text: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
+                            status: 'received',
+                            check: 'na',
                         },
                         {
                             date: '20/03/2020 16:35:00',
                             text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            status: 'sent',
+                            check: 'received',
                         }
                     ]
                 },
@@ -69,17 +76,20 @@ const app = new Vue(
                         {
                             date: '28/03/2020 10:10:40',
                             text: 'La Marianna va in campagna',
-                            status: 'received'
+                            status: 'received',
+                            check: 'na',
                         },
                         {
                             date: '28/03/2020 10:20:10',
                             text: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            status: 'sent',
+                            check: 'received'
                         },
                         {
                             date: '28/03/2020 16:15:22',
                             text: 'Ah scusa!',
-                            status: 'received'
+                            status: 'received',
+                            check: 'na',
                         }
                     ]
                 },
@@ -92,17 +102,20 @@ const app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             text: 'Sei andato in palestra oggi?',
-                            status: 'sent'
+                            status: 'sent',
+                            check: 'received'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             text: 'Certo!!! Tu piuttosto, che fine hai fatto?',
-                            status: 'received'
+                            status: 'received',
+                            check: 'na'
                         },
                         {
                             date: '10/01/2020 15:30:55',
                             text: 'Appuntamento dal dentista',
-                            status: 'sent'
+                            status: 'sent',
+                            check: 'received'
                         },
                     ]
                 },
@@ -115,12 +128,14 @@ const app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             text: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            status: 'sent',
+                            check: 'received'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             text: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
+                            status: 'received',
+                            check: 'na'
                         }
                     ]
                 },
@@ -133,12 +148,14 @@ const app = new Vue(
                         {
                             date: '19/04/2021 18:30:25',
                             text: 'Bella la tua foto profilo!',
-                            status: 'sent'
+                            status: 'sent',
+                            check: 'received'
                         },
                         {
                             date: '19/04/2021 19:00:00',
                             text: 'Grazie, l\'ho scattata ad una festa',
-                            status: 'received'
+                            status: 'received',
+                            check: 'na'
                         }
                     ]
                 },
@@ -151,12 +168,14 @@ const app = new Vue(
                         {
                             date: '04/06/2020 11:45:50',
                             text: 'Vieni alla festa di Stefania?',
-                            status: 'sent'
+                            status: 'sent',
+                            check: 'received'
                         },
                         {
                             date: '04/06/2020 12:05:20',
                             text: 'Si, certo! Vedrai, ci divertiremo come pazzi',
-                            status: 'received'
+                            status: 'received',
+                            check: 'received'
                         }
                     ]
                 }
@@ -175,6 +194,7 @@ const app = new Vue(
             ]
         },
 
+        //filter the contact list
         computed : {
             filteredContacts : function (){
                 return this.contacts.filter((element)=>{
@@ -185,33 +205,43 @@ const app = new Vue(
 
         methods: {
 
+            //when select a contact, it will appear on the right, name + all messages
             show : function (index){
                 this.counter = index
             },
-                
+
+            //send a message, receive a random answer (before the answer-message appear, "is writing" under the contact name)
             send : function(array) {
 
                 if(this.newMessage != ''){
-                    array.push({
+                   array.push({
                         date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
                         text: this.newMessage,
-                        status: 'sent'
-                    });                    
+                        status: 'sent',
+                    }); 
+
+                    setTimeout(function(){ 
+                        array.forEach(element => {
+                            if (element.status != 'received') {
+                                element.check = 'received'
+                            }                     
+                        })
+                    }, 2000);
+
                 }
 
                 this.newMessage = "";
 
-                let isWriting = 3;
-
+                // Set variables for random answers
                 let RandomText = this.randomAnswers[Math.floor(Math.random() * this.randomAnswers.length)];
-
                 let randomEmoji = String.fromCodePoint(this.randomIcon[Math.floor(Math.random() * this.randomIcon.length)]);
 
                 //Milestone 3 + bonus : adding is writing.../ random answer from the array "randomAnswers"
+                let isWriting = 3;
+
                 let answer = setInterval(function(){
                     
                     if (isWriting == 0) {
-
                         clearInterval(answer);
 
                         array.push({
@@ -221,14 +251,14 @@ const app = new Vue(
                         });
 
                         document.querySelector('.last-access').innerHTML= 'Ultimo accesso oggi alle ' + dayjs().format('HH:mm');
-            
+
                     }else{
                         document.querySelector('.last-access').innerHTML= 'Sta scrivendo...';
                     }
 
                     isWriting -= 1;
-
                 },1000);
+
 
                 //Milestone 3: After 1 second, received a default answer(OK)  
                 // let answer = setTimeout(function(){ 
@@ -240,15 +270,16 @@ const app = new Vue(
                 // }, this.time);
             },
 
-
+            //toggle for dropdown menu of each message
             toggle: function(index){
                 this.indexCheck = index
                 this.active = !this.active;
             },
-          
+
+            //delete message
             deleteMessage : function(array, index){
                 array.splice(index,1)
-            }
+            }              
 
         }
 
